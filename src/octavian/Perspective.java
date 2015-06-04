7f3ac8d11c9af59d94e444827757a0e21eab17ed
Dmitry Ponyatov <dponyatov@gmail.com>
2015-06-04 09:24:33 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/octavian/Perspective.java
+++ src/octavian/Perspective.java
@@ -3,11 +3,12 @@ package octavian;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
-public class perspective implements IPerspectiveFactory {
+public class Perspective implements IPerspectiveFactory {
 
-	@Override
 	public void createInitialLayout(IPageLayout layout) {
-		// TODO Auto-generated method stub
+		layout.setEditorAreaVisible(false);
+		layout.setFixed(true);
+		
 	}
 
 }
