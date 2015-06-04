c7e8daa1d053ed3441efbbb8c2ba07bd9ebea3dd
Dmitry Ponyatov <dponyatov@gmail.com>
2015-06-04 08:29:36 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ src/org/eclipse/ponyatov/octavian/perspective.java
@@ -0,0 +1,13 @@
+package org.eclipse.ponyatov.octavian;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class perspective implements IPerspectiveFactory {
+
+	@Override
+	public void createInitialLayout(IPageLayout layout) {
+		// TODO Auto-generated method stub
+	}
+
+}
